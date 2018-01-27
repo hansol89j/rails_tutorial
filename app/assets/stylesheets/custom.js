8ad1c735e616dd4dd72c5a3da7dc95a58006258f
Hansol Jeong <hansol89j@gmail.com>
2018-01-27 16:12:53 +0900

    Finish layout and routes

---
+++ app/assets/stylesheets/custom.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  // universal
+  'body': {
+    'paddingTop': [{ 'unit': 'px', 'value': 60 }]
+  },
+  'section': {
+    'overflow': 'auto'
+  },
+  'textarea': {
+    'resize': 'vertical'
+  },
+  'center': {
+    'textAlign': 'center'
+  },
+  'center h1': {
+    'marginBottom': [{ 'unit': 'px', 'value': 10 }]
+  }
+});
